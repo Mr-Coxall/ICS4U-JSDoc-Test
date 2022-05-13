ea7f59bbc02ce6f7887811878b596560f728d8da
Patrick Coxall <mr.coxall@mths.ca>
2022-05-13 15:35:57 +0000

    Initial commit

---
+++ .build/index.js
@@ -0,0 +1,8 @@
+function log(someMessage) {
+  console.log(someMessage);
+  return 1;
+}
+let message = "Hello, World!";
+let returnValue = log(message);
+console.log("\nDone.");
+//# sourceMappingURL=index.js.map
